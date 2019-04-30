2aa463f2cf2c88ca578fc3feb2fc5c4886ca3268
Cwekii <dominik.cvetkovski@gmail.com>
2019-04-30 11:18:06 +0200

    Shooting

---
+++ Vjezbe na nastavi/Shooting/Bullet_Destory.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullet_Destory : MonoBehaviour
+{
+    public float vrijeme_destroy = 5.5f;
+    void Update()
+    {
+        vrijeme_destroy -= Time.deltaTime;
+        if(vrijeme_destroy <= 0)
+        {
+            Destroy(this.gameObject);
+        }
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        Destroy(other.gameObject);
+        Destroy(this.gameObject);
+    }
+}
---
+++ Vjezbe na nastavi/Shooting/ChageWeapon.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChageWeapon : MonoBehaviour
+{
+    public GameObject gun;
+    public GameObject kalas;
+
+    void Start()
+    {
+        gun.SetActive(true);
+        kalas.SetActive(false);
+    }
+
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Alpha1))
+        {
+            gun.SetActive(true);
+            kalas.SetActive(false);
+        }
+        if (Input.GetKeyDown(KeyCode.Alpha2))
+        {
+            gun.SetActive(false);
+            kalas.SetActive(true);
+        }
+    }
+}
